7f5388cd277be3e4ff48a6856ec43aad369e65ea
Kanmii <maneptha@gmail.com>
2019-12-16 12:48:25 +0100

    Initial commit.

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script type="text/typescript">
+  import { data } from "./app-utils";
+  export let name: string;
+</script>
+
+<main id="app-main">Hello {name}</main>
+<span id="app-count">{data.count}</span>
